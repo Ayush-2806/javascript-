9dece35e59aa5a9f202339d4a25a10552283f082
AYUSH AGRAWAL2806 <127664211+Ayush-2806@users.noreply.github.com>
2023-04-22 14:33:29 +0530

    Add files via upload

---
+++ Project2/check.js
@@ -0,0 +1,36 @@
+
+const box = document.getElementById("box1")
+function che(){
+  box.style.backgroundColor = "black";
+  box.style.transition = "2s";
+}
+
+function a(){
+    box.style.backgroundColor = "blue";
+    box.style.transition = "2s";
+  }
+
+function b(){
+    box.style.backgroundColor = "orange";
+    box.style.transition = "2s";
+  }
+
+function c(){
+    box.style.backgroundColor = "green";
+    box.style.transition = "2s";
+  }
+
+function d(){
+    box.style.backgroundColor = "white";
+    box.style.transition = "2s";
+  }
+
+function e(){
+    box.style.backgroundColor = "gray";
+    box.style.transition = "2s";
+  }
+
+function f(){
+    box.style.backgroundColor = "voilet";
+    box.style.transition = "2s";
+  }
